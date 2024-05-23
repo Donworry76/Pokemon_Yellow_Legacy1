f111e2ff8e990c57f0e215c2418bb14f0dd42e12
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2024-05-23 17:22:33 +0200

    Update Route22.asm

--- data/wild/maps/Route22.asm
+++ data/wild/maps/Route22.asm
@@ -1,6 +1,6 @@
 Route22WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db  3, NIDORAN_M
+	db  3, CUBONE
 	db  4, MANKEY
 	db  3, NIDORAN_F
 	db  4, NIDORAN_M
@@ -9,7 +9,7 @@ Route22WildMons:
 	db  5, MANKEY
 	db  6, SPEAROW
 	db  5, RATTATA
-	db  5, RATTATA
+	db  5, DITTO
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
