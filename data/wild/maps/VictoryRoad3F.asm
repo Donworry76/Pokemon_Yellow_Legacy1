6801d8092eb00dac9c7de78ffc48e864007a05cf
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2024-05-23 17:32:42 +0200

    Update VictoryRoad3F.asm

--- data/wild/maps/VictoryRoad3F.asm
+++ data/wild/maps/VictoryRoad3F.asm
@@ -1,11 +1,11 @@
 VictoryRoad3FWildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 47, GRAVELER
-	db 43, GEODUDE
+	db 47, NIDOQUEEN
+	db 43, NIDOKING
 	db 46, MACHAMP
-	db 47, GOLBAT
+	db 47, DUGTRIO
 	db 45, GOLEM
-	db 46, ONIX
+	db 46, HAUNTER
 	db 46, RHYDON
 	db 48, HITMONCHAN
 	db 48, HITMONLEE
