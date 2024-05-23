fe1085b72c8a5c23c96238333b29ac88d3079a44
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2024-05-23 17:21:58 +0200

    Update Route21.asm

--- data/wild/maps/Route21.asm
+++ data/wild/maps/Route21.asm
@@ -1,22 +1,22 @@
 Route21WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db 25, PIDGEY
-	db 23, RATTATA
-	db 23, PIDGEY
-	db 21, PIDGEY
+	db 25, TANGELA
+	db 23, ARBOK
+	db 23, SANDSLASH
+	db 21, DODUO
 	db 27, MR_MIME
-	db 25, RATTATA
+	db 25, VILEPLUME
 	db 25, RATICATE
-	db 27, PIDGEOTTO
+	db 27, VICTREBEEL
 	db 29, PIDGEOTTO
 	db 30, PIDGEOT
 	end_grass_wildmons
 
 	def_water_wildmons 5 ; encounter rate
-	db 20, TENTACOOL
+	db 20, SEEL
 	db 10, TENTACOOL
-	db 15, TENTACOOL
-	db  5, TENTACOOL
+	db 15, HORSEA
+	db  5, MAGIKARP
 	db 15, TENTACOOL
 	db 20, TENTACOOL
 	db 25, TENTACOOL
