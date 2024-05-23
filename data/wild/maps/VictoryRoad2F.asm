8e339386353fa796ca7ac64eacee336c2c32383a
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2024-05-23 17:31:58 +0200

    Update VictoryRoad2F.asm

--- data/wild/maps/VictoryRoad2F.asm
+++ data/wild/maps/VictoryRoad2F.asm
@@ -1,12 +1,12 @@
 VictoryRoad2FWildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db 46, GRAVELER
-	db 42, RHYHORN
-	db 45, GRAVELER
+	db 46, VENOMOTH
+	db 42, KADABRA
+	db 45, VAPOREON
 	db 46, MACHAMP
 	db 44, GOLBAT
 	db 45, GOLEM
-	db 45, ONIX
+	db 45, VICTREBELL
 	db 47, RHYDON
 	db 48, HITMONLEE
 	db 48, HITMONLEE
