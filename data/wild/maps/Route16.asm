8347dee10d6d97be6460e9486af3023e9194cf5e
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2024-05-23 17:16:18 +0200

    Update Route16.asm

--- data/wild/maps/Route16.asm
+++ data/wild/maps/Route16.asm
@@ -1,12 +1,12 @@
 Route16WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db 22, SPEAROW
+	db 22, PONYTA
 	db 22, DODUO
-	db 23, RATTATA
+	db 23, RHYHORN
 	db 24, DODUO
 	db 24, GRIMER
-	db 26, DODUO
-	db 23, SPEAROW
+	db 26, RATICATE
+	db 23, SANDSLASH
 	db 24, FEAROW
 	db 25, RATICATE
 	db 26, RATICATE
