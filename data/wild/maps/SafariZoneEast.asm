3a254b768336cb15e74476e1f503b49a1b7a9a4b
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2024-05-23 17:35:28 +0200

    Update SafariZoneEast.asm

--- data/wild/maps/SafariZoneEast.asm
+++ data/wild/maps/SafariZoneEast.asm
@@ -1,15 +1,15 @@
 SafariZoneEastWildMons:
 	def_grass_wildmons 30 ; encounter rate
 	db 32, TAUROS
-	db 33, RHYHORN
+	db 33, GOLDUCK
 	db 35, EXEGGCUTE
 	db 34, TAUROS
-	db 36, CUBONE
-	db 36, MAROWAK
+	db 36, KANGASKHAN
+	db 36, NIDOQUEEN
 	db 36, SCYTHER
 	db 37, PINSIR
 	db 35, CHANSEY
-	db 40, SCYTHER
+	db 40, GOLEM
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
