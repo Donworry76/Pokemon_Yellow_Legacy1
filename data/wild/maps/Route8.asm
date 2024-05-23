796e5b3236a9eafc7bcdc5d819992da12c3d7698
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2024-05-23 17:10:09 +0200

    Update Route8.asm

--- data/wild/maps/Route8.asm
+++ data/wild/maps/Route8.asm
@@ -2,14 +2,14 @@ Route8WildMons:
 	def_grass_wildmons 15 ; encounter rate
 	db 20, GROWLITHE
 	db 22, PIDGEY
-	db 20, RATTATA
+	db 20, KOFFING
 	db 17, ABRA
-	db 18, ABRA
+	db 18, LICKITUNG
 	db 24, PIDGEOTTO
-	db 19, ABRA
+	db 19, GRIMER
 	db 24, JIGGLYPUFF
 	db 24, KADABRA
-	db 28, KADABRA
+	db 28, POLIWHIRL
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
