e2abd94ff066c13e158b48730aa5204052118dc5
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2024-05-23 17:13:42 +0200

    Update Route12.asm

--- data/wild/maps/Route12.asm
+++ data/wild/maps/Route12.asm
@@ -1,15 +1,15 @@
 Route12WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 25, ODDISH
-	db 25, BELLSPROUT
-	db 28, PIDGEY
+	db 25, TANGELA
+	db 25, LICKITUNG
+	db 28, PARASECT
 	db 28, PIDGEOTTO
-	db 28, ODDISH
+	db 28, FEAROW
 	db 29, TANGELA
 	db 29, GLOOM
 	db 29, WEEPINBELL
 	db 26, FARFETCHD
-	db 31, FARFETCHD
+	db 31, FLAREON
 	end_grass_wildmons
 
 	def_water_wildmons 3 ; encounter rate
