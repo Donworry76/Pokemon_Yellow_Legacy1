fbca4d31b416e3d04b8468503d430f4761578ac1
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2024-05-23 17:07:05 +0200

    Update Route5.asm

--- data/wild/maps/Route5.asm
+++ data/wild/maps/Route5.asm
@@ -1,15 +1,15 @@
 Route5WildMons:
 	def_grass_wildmons 15 ; encounter rate
 	db 15, MEOWTH
-	db 14, RATTATA
+	db 14, DITTO
 	db 13, ABRA
 	db 16, FARFETCHD
-	db 16, RATTATA
-	db 17, PIDGEY
+	db 16, PARAS
+	db 17, DODUO
 	db 17, PIDGEOTTO
 	db 13, JIGGLYPUFF
-	db 15, JIGGLYPUFF
-	db 17, JIGGLYPUFF
+	db 15, LICKITUNG
+	db 17, TANGELA
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
