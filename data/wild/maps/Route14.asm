424c4dbb97f50b9e9f0fde9321a93926ed7d9e49
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2024-05-23 17:15:12 +0200

    Update Route14.asm

--- data/wild/maps/Route14.asm
+++ data/wild/maps/Route14.asm
@@ -1,14 +1,14 @@
 Route14WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 26, ODDISH
-	db 26, BELLSPROUT
+	db 26, MACHOKE
+	db 26, PONYTA
 	db 24, WEEPINBELL
 	db 30, PIDGEOTTO
 	db 28, GLOOM
 	db 28, SCYTHER
 	db 30, GLOOM
-	db 28, VENONAT
-	db 27, VENONAT
+	db 28, PARASECT
+	db 27, DODRIO
 	db 33, VENOMOTH
 	end_grass_wildmons
 
