f51c08c6941aed2f1cc0b82ea1a5323f3388c258
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2024-05-23 17:29:00 +0200

    Update PowerPlant.asm

--- data/wild/maps/PowerPlant.asm
+++ data/wild/maps/PowerPlant.asm
@@ -1,14 +1,14 @@
 PowerPlantWildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db 30, MAGNEMITE
+	db 30, ELECTRODE
 	db 33, MAGNETON
 	db 35, MAGNETON
-	db 33, VOLTORB
+	db 33, ELECTABUZZ
 	db 37, ELECTRODE
 	db 33, GRIMER
 	db 37, MUK
 	db 38, ELECTABUZZ
-	db 39, ELECTABUZZ
+	db 39, PORYGON
 	db 33, RAICHU
 	end_grass_wildmons
 
