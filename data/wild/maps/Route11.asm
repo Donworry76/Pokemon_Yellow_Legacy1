22762d191d5a8cc69269bd5c052edcaf8537a0e5
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2024-05-23 17:12:57 +0200

    Update Route11.asm

--- data/wild/maps/Route11.asm
+++ data/wild/maps/Route11.asm
@@ -1,15 +1,15 @@
 Route11WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 16, PIDGEY
-	db 15, RATTATA
+	db 16, CUBONE
+	db 15, DODUO
 	db 18, SANDSHREW
 	db 17, DROWZEE
 	db 17, EKANS
 	db 18, DROWZEE
 	db 20, PIDGEOTTO
 	db 19, MAGNEMITE
-	db 19, DROWZEE
-	db 20, DROWZEE
+	db 19, VULPIX
+	db 20, SEEL
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
