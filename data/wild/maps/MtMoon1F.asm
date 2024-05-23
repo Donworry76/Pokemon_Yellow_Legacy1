a2d20727847f17167f8ed1721ef14463f0766a64
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2024-05-23 17:25:33 +0200

    Update MtMoon1F.asm

--- data/wild/maps/MtMoon1F.asm
+++ data/wild/maps/MtMoon1F.asm
@@ -1,15 +1,15 @@
 MtMoon1FWildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db  8, ZUBAT
-	db  9, ZUBAT
+	db  8, CLEFAIRY
+	db  9, MACHOP
 	db 10, GEODUDE
-	db  6, ZUBAT
+	db  6, PARAS
 	db  7, ZUBAT
 	db 10, SANDSHREW
 	db 10, GEODUDE
-	db 11, SANDSHREW
-	db 12, SANDSHREW
-	db 12, CLEFAIRY
+	db 11, DIGLETT
+	db 12, ONIX
+	db 12, RHYHORN
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
