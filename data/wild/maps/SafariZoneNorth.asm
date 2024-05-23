d3877bd72be20ffb3ad38d1117c888c9a3bb2b0c
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2024-05-23 17:36:46 +0200

    Update SafariZoneNorth.asm

--- data/wild/maps/SafariZoneNorth.asm
+++ data/wild/maps/SafariZoneNorth.asm
@@ -2,14 +2,14 @@ SafariZoneNorthWildMons:
 	def_grass_wildmons 30 ; encounter rate
 	db 32, NIDOQUEEN
 	db 33, NIDOKING
-	db 35, CUBONE
+	db 35, MAGMAR
 	db 34, KANGASKHAN
 	db 34, RHYHORN
-	db 36, MAROWAK
+	db 36, MACHAMP
 	db 35, KANGASKHAN
-	db 35, RHYHORN
+	db 35, ELECTABUZZ
 	db 36, RHYHORN
-	db 40, CHANSEY
+	db 40, VICTREEBEL
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
