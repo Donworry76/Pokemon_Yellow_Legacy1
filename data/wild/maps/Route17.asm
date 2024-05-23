ae1abbad38719f77a587b439fd5b102f6bc433a9
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2024-05-23 17:16:54 +0200

    Update Route17.asm

--- data/wild/maps/Route17.asm
+++ data/wild/maps/Route17.asm
@@ -2,13 +2,13 @@ Route17WildMons:
 	def_grass_wildmons 25 ; encounter rate
 	db 26, DODUO
 	db 27, FEAROW
-	db 27, DODUO
+	db 27, VOLTORB
 	db 28, GRIMER
 	db 28, PONYTA
-	db 29, GRIMER
+	db 29, KOFFING
 	db 29, FEAROW
-	db 28, DODUO
-	db 32, PONYTA
+	db 28, GRAVELER
+	db 32, WEEZING
 	db 31, DODRIO
 	end_grass_wildmons
 
