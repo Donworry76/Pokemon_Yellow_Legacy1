305abdf7b9fad3b37bd70b9926cbfed9e4b978a0
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2024-05-23 17:24:41 +0200

    Update Route25.asm

--- data/wild/maps/Route25.asm
+++ data/wild/maps/Route25.asm
@@ -1,14 +1,14 @@
 Route25WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 12, PIDGEY
-	db 12, BELLSPROUT
+	db 12, MANKEY
+	db 12, BUTTERFREE
 	db 13, ABRA
 	db 14, ODDISH
 	db 14, BELLSPROUT
-	db 15, PIDGEY
-	db 13, VENONAT
+	db 15, BEEDRILL
+	db 13, PIDGEOTTO
 	db 16, VENONAT
-	db 17, PIDGEY
+	db 17, SEEL
 	db 17, PIDGEOTTO
 	end_grass_wildmons
 
