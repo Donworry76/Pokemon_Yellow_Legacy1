b17dd7b54c7945d2b3033e8b14d734d7b3ee9a97
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2024-05-23 17:31:15 +0200

    Update VictoryRoad1F.asm

--- data/wild/maps/VictoryRoad1F.asm
+++ data/wild/maps/VictoryRoad1F.asm
@@ -1,13 +1,13 @@
 VictoryRoad1FWildMons:
 	def_grass_wildmons 15 ; encounter rate
 	db 46, GRAVELER
-	db 42, RHYHORN
-	db 45, GRAVELER
+	db 42, PERSIAN
+	db 45, KANGASKHAN
 	db 46, MACHAMP
 	db 44, GOLBAT
 	db 46, GOLEM
-	db 43, ONIX
-	db 45, ONIX
+	db 43, ELECTRODE
+	db 45, NIDOKING
 	db 48, HITMONCHAN
 	db 48, HITMONCHAN
 	end_grass_wildmons
