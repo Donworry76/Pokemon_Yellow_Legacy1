4ea7da0321caadfa7009147f1faf608a4dd2fc12
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2024-05-23 17:11:12 +0200

    Update Route9.asm

--- data/wild/maps/Route9.asm
+++ data/wild/maps/Route9.asm
@@ -1,14 +1,14 @@
 Route9WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 16, NIDORAN_M
-	db 16, NIDORAN_F
-	db 18, RATTATA
+	db 16, NIDORINO
+	db 16, NIDORINA
+	db 18, BEEDRILL
 	db 18, LICKITUNG
-	db 18, NIDORINA
-	db 18, NIDORINO
-	db 18, SPEAROW
+	db 18, TANGELA
+	db 18, GLOOM
+	db 18, BUTTERFREE
 	db 20, FEAROW
-	db 21, PINSIR
+	db 21, SCYTHER
 	db 23, PINSIR
 	end_grass_wildmons
 
