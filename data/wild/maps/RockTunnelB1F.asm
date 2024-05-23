68bd1566224bb3f169ceec5484a41f57d212acb8
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2024-05-23 17:28:21 +0200

    Update RockTunnelB1F.asm

--- data/wild/maps/RockTunnelB1F.asm
+++ data/wild/maps/RockTunnelB1F.asm
@@ -1,15 +1,15 @@
 RockTunnelB1FWildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 20, ZUBAT
+	db 20, VOLTORB
 	db 17, GEODUDE
 	db 18, MACHOP
-	db 21, ZUBAT
-	db 22, ZUBAT
+	db 21, GLOOM
+	db 22, GOLBAT
 	db 21, GEODUDE
-	db 20, MACHOP
+	db 20, GLOOM
 	db 17, ONIX
-	db 18, ONIX
-	db 22, ONIX
+	db 18, MACHOKE
+	db 22, PRIMEAPE
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
