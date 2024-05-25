2b23dff812550186a68090fbf35c196a514fd7d6
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2024-05-25 17:05:31 +0200

    Update PokemonMansion2F.asm

--- data/wild/maps/PokemonMansion2F.asm
+++ data/wild/maps/PokemonMansion2F.asm
@@ -1,14 +1,14 @@
 PokemonMansion2FWildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db 37, RATTATA
-	db 37, RATICATE
+	db 37, MAGMAR
+	db 37, WEEZING
 	db 36, GRIMER
 	db 39, GRIMER
 	db 40, MUK
 	db 40, RATICATE
 	db 42, MUK
-	db 40, MAGMAR
-	db 42, MAGMAR
+	db 39, OMANYTE
+	db 39, KABUTOPS
 	db 45, MAGMAR
 	end_grass_wildmons
 
