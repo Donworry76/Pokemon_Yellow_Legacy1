bcea5ffdacdae164a771d5d9f992aeac918936fe
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2024-05-25 17:09:27 +0200

    Update ViridianForest.asm

--- data/wild/maps/ViridianForest.asm
+++ data/wild/maps/ViridianForest.asm
@@ -2,13 +2,13 @@ ViridianForestWildMons:
 	def_grass_wildmons 25 ; encounter rate
 	db  4, CATERPIE
 	db  4, WEEDLE
-	db  7, PIDGEY
+	db  7, EXEGGCUTE
 	db  7, KAKUNA
 	db  5, ODDISH
 	db  6, METAPOD
-	db  6, ODDISH
+	db  6, BELLSPROUT
 	db  7, KAKUNA
-	db  8, PIDGEY
+	db  8, VENONAT
 	db  9, PIDGEOTTO
 	end_grass_wildmons
 
