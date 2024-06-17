e43b819526f6bb94cb1c14b0e86b68efe8d0a660
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2024-06-17 15:22:30 +0200

    Update hitmonchan.asm

--- data/pokemon/base_stats/hitmonchan.asm
+++ data/pokemon/base_stats/hitmonchan.asm
@@ -10,7 +10,7 @@
 	INCBIN "gfx/pokemon/front/hitmonchan.pic", 0, 1 ; sprite dimensions
 	dw HitmonchanPicFront, HitmonchanPicBack
 
-	db COMET_PUNCH, AGILITY, MEDITATE, NO_MOVE ; level 1 learnset
+	db COMET_PUNCH, AGILITY, MEDITATE, KARATE_CHOP ; level 1 learnset
 	db GROWTH_MEDIUM_FAST ; growth rate
 
 	; tm/hm learnset
