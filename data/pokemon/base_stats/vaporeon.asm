fbdf635893de3722f79ef6fbc4924e9a5a5b3eef
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2025-01-01 14:20:32 +0100

    Update vaporeon.asm

--- data/pokemon/base_stats/vaporeon.asm
+++ data/pokemon/base_stats/vaporeon.asm
@@ -10,7 +10,7 @@
 	INCBIN "gfx/pokemon/front/vaporeon.pic", 0, 1 ; sprite dimensions
 	dw VaporeonPicFront, VaporeonPicBack
 
-	db QUICK_ATTACK, WATER_GUN, SAND_ATTACK, NO_MOVE ; level 1 learnset
+	db QUICK_ATTACK, BUBBLE, SAND_ATTACK, NO_MOVE ; level 1 learnset
 	db GROWTH_MEDIUM_FAST ; growth rate
 
 	; tm/hm learnset
