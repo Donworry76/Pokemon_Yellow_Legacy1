e4c552e2368c71c1af166200bbd6254f24855cff
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2025-01-12 12:33:52 +0100

    Update voltorb.asm

--- data/pokemon/base_stats/voltorb.asm
+++ data/pokemon/base_stats/voltorb.asm
@@ -10,7 +10,7 @@
 	INCBIN "gfx/pokemon/front/voltorb.pic", 0, 1 ; sprite dimensions
 	dw VoltorbPicFront, VoltorbPicBack
 
-	db THUNDERSHOCK, SCREECH, NO_MOVE, NO_MOVE ; level 1 learnset
+	db THUNDERSHOCK, SCREECH, SWIFT, NO_MOVE ; level 1 learnset
 	db GROWTH_MEDIUM_FAST ; growth rate
 
 	; tm/hm learnset
