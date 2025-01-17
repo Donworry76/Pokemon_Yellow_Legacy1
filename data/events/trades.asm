e0f2294e1b68e2992e760e890ce75904d0be22f9
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2025-01-17 17:38:45 +0100

    Update trades.asm

--- data/events/trades.asm
+++ data/events/trades.asm
@@ -6,14 +6,14 @@ TradeMons:
 	; from the Japanese Blue trades, which used species that evolve.
 	; Japanese Red and Green used TRADE_DIALOGSET_CASUAL, and had
 	; the same species as English Red and Blue.
-	db LICKITUNG,  DUGTRIO,  TRADE_DIALOGSET_CASUAL,    "GURIO@@@@@@"
+	db ODDISH,  PONYTA,  	 TRADE_DIALOGSET_CASUAL,    "GURIO@@@@@@"
 	db CLEFAIRY,   MR_MIME,  TRADE_DIALOGSET_CASUAL,    "MILES@@@@@@"
 	db BUTTERFREE, BEEDRILL, TRADE_DIALOGSET_HAPPY,     "STINGER@@@@" ; unused
-	db KANGASKHAN, MUK,      TRADE_DIALOGSET_CASUAL,    "STICKY@@@@@"
+	db ALAKAZAM, GENGAR,      TRADE_DIALOGSET_CASUAL,   "STICKY@@@@@"
 	db MEW,        MEW,      TRADE_DIALOGSET_HAPPY,     "BART@@@@@@@" ; unused
-	db TANGELA,    PARASECT, TRADE_DIALOGSET_CASUAL,    "SPIKE@@@@@@"
+	db TAUROS,    LICKITUNG, TRADE_DIALOGSET_CASUAL,    "SPIKE@@@@@@"
 	db POLIWHIRL,  JYNX,     TRADE_DIALOGSET_EVOLUTION, "LOLA@@@@@@@"
-	db GOLDUCK,    RHYDON,   TRADE_DIALOGSET_EVOLUTION, "BUFFY@@@@@@"
-	db GROWLITHE,  DEWGONG,  TRADE_DIALOGSET_HAPPY,     "CEZANNE@@@@"
-	db CUBONE,     MACHOKE,  TRADE_DIALOGSET_HAPPY,     "RICKY@@@@@@"
+	db MEW,    MEWTWO,   TRADE_DIALOGSET_EVOLUTION, "BUFFY@@@@@@"
+	db LAPRAS,     SNORLAX,  TRADE_DIALOGSET_HAPPY,     "CEZANNE@@@@"
+	db VOLTORB,     TANGELA, TRADE_DIALOGSET_HAPPY,     "RICKY@@@@@@"
 	assert_table_length NUM_NPC_TRADES
