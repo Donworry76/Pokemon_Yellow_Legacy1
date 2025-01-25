dd3d274d521fbda300e0f20dd29fc9cf5b2b0fe5
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2025-01-25 20:02:17 +0100

    Update seaking.asm

--- data/pokemon/base_stats/seaking.asm
+++ data/pokemon/base_stats/seaking.asm
@@ -1,16 +1,16 @@
 	db DEX_SEAKING ; pokedex id
 
-	db 100, 102,  65,  68,  90
+	db 110, 82,  65,  68,  100
 	;   hp  atk  def  spd  spc
 
-	db WATER, NORMAL ; type
+	db WATER, FIRE ; type
 	db 60 ; catch rate
 	db 170 ; base exp
 
 	INCBIN "gfx/pokemon/front/seaking.pic", 0, 1 ; sprite dimensions
 	dw SeakingPicFront, SeakingPicBack
 
-	db TAIL_WHIP, BUBBLE, NO_MOVE, NO_MOVE ; level 1 learnset
+	db FIRE_SPIN, BUBBLE, NO_MOVE, NO_MOVE ; level 1 learnset
 	db GROWTH_MEDIUM_FAST ; growth rate
 
 	; tm/hm learnset
